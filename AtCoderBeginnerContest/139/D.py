1c3c2c655dc1db40e0499091e217c6349051ddab
corutopi <39874652+corutopi@users.noreply.github.com>
2020-08-25 01:42:54 +0900

    solve ABC 139 D.

---
+++ AtCoderBeginnerContest/139/D.py
@@ -0,0 +1,20 @@
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# import bisect
+# from collections import deque
+# from decorator import stop_watch
+#
+#
+# @stop_watch
+def solve(N):
+    n = N - 1
+    print((n + 1) * n // 2)
+
+
+if __name__ == '__main__':
+    # S = input()
+    N = int(input())
+    # N, M = map(int, input().split())
+    # As = [int(i) for i in input().split()]
+    # Bs = [int(i) for i in input().split()]
+    solve(N)
