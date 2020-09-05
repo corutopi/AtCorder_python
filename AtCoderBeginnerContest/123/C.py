63cdd1b0558b0171ce5e216a04b74d5663ec4d9d
corutopi <39874652+corutopi@users.noreply.github.com>
2020-09-06 01:53:08 +0900

    solve ABC 123 A, C. solve ABC 124 D. solve ABC 128 D.

---
+++ AtCoderBeginnerContest/123/C.py
@@ -0,0 +1,28 @@
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# import bisect
+# from collections import deque
+# from decorator import stop_watch
+#
+#
+# @stop_watch
+def solve(N, A, B, C, D, E):
+    tmp = min(A, B, C, D, E)
+    tmp, tmp2 = divmod(N, tmp)
+    tmp += 0 if tmp2 == 0 else 1
+    print(tmp + 4)
+
+
+if __name__ == '__main__':
+    N = int(input())
+    A = int(input())
+    B = int(input())
+    C = int(input())
+    D = int(input())
+    E = int(input())
+    solve(N, A, B, C, D, E)
+
+    # # test
+    # from random import randint
+    # from func import random_str
+    # solve()
