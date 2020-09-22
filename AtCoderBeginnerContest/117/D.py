088485be0942328ed81a680f5ea59c5d4c66644c
corutopi <39874652+corutopi@users.noreply.github.com>
2020-09-22 13:19:32 +0900

    solve ABC 117 D.

---
+++ AtCoderBeginnerContest/117/D.py
@@ -0,0 +1,36 @@
+# 解説を参考に作成
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# import bisect
+# from collections import deque
+# from decorator import stop_watch
+#
+#
+# @stop_watch
+def solve(N, K, A):
+    import math
+    max_K = 0
+    for i in reversed(list(range(int(math.log2(K + 1)) + 1))):
+        cnt = 0
+        for a in A:
+            if a >> i & 1:
+                cnt += 1
+        if cnt <= N // 2:
+            if max_K + (1 << i) <= K:
+                max_K += 1 << i
+    ans = 0
+    for a in A:
+        ans += a ^ max_K
+    print(ans)
+
+
+
+if __name__ == '__main__':
+    N, K = map(int, input().split())
+    A = [int(i) for i in input().split()]
+    solve(N, K, A)
+
+    # # test
+    # from random import randint
+    # from func import random_str
+    # solve()
