e073f3fc6340d3ecafb5b428f3f47e40f69af701
corutopi <39874652+corutopi@users.noreply.github.com>
2020-10-05 23:06:51 +0900

    solve ABC 099 C.

---
+++ AtCoderBeginnerContest/099/C.py
@@ -0,0 +1,32 @@
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# import bisect
+# from collections import deque
+# from decorator import stop_watch
+#
+#
+# @stop_watch
+def solve(N):
+    import math
+    coins = [1] + \
+            [6 ** i for i in range(1, int(math.log(N, 6)) + 2)] + \
+            [9 ** i for i in range(1, int(math.log(N, 9)) + 2)]
+    coins.sort()
+    dp = [0] + [math.inf] * N
+    for i in range(1, N + 1):
+        for c in coins:
+            if i - c < 0:
+                break
+            dp[i] = min(dp[i], dp[i - c] + 1)
+    print(dp[N])
+
+
+if __name__ == '__main__':
+    N = int(input())
+    solve(N)
+
+    # # test
+    # from random import randint
+    # from func import random_str
+    # for i in range(1, 100 + 1):
+    #     solve(i)
