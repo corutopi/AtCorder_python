5d7481623d16ed821df4d8abc353fb19f7247f50
corutopi <39874652+corutopi@users.noreply.github.com>
2020-11-11 02:10:27 +0900

    solve zenkokutouitsu_programming_king_qualify C(refactoring).

--- other/nikkei_programing_king/C.py
+++ other/nikkei_programing_king/C.py
@@ -1,13 +1,32 @@
-"""
-C - Different Strokes
-"""
-
-
-def solve():
-    # A, Bともに残っている料理の中で2人の幸福度の和が最大となる料理を食べる.
-    N = int(input())
-    dish_sum_list = 10
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# import bisect
+# from collections import deque
+# from decorator import stop_watch
+#
+#
+# @stop_watch
+def solve(N, AB):
+    takahashi = 0
+    aoki = 0
+    for ab in AB:
+        ab.append(sum(ab))
+    AB.sort(key=lambda x: x[2], reverse=True)
+    for i in range(N):
+        if i % 2 == 0:
+            takahashi += AB[i][0]
+        else:
+            aoki += AB[i][1]
+    print(takahashi - aoki)
 
 
 if __name__ == '__main__':
-    solve()
+    # S = input()
+    N = int(input())
+    AB = [[int(i) for i in input().split()] for _ in range(N)]
+    solve(N, AB)
+
+    # # test
+    # from random import randint
+    # from func import random_str, random_ints
+    # solve()
