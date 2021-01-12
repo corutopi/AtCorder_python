2dec9b8eaa048dae0daf9b6cd04704a193431cf0
corutopi <39874652+corutopi@users.noreply.github.com>
2021-01-13 00:50:36 +0900

    solve ARC 032 B.

---
+++ AtCoderRegularContest/032/B.py
@@ -0,0 +1,85 @@
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# import bisect
+# from collections import deque
+inf = float('inf')
+mod = 10 ** 9 + 7
+mod2 = 998244353
+
+class UnionFind:
+    """
+    下記から拝借
+    https://note.nkmk.me/python-union-find/
+    """
+
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        return {r: self.members(r) for r in self.roots()}
+
+    def __str__(self):
+        return '\n'.join(
+            '{}: {}'.format(r, self.members(r)) for r in self.roots())
+
+
+# from decorator import stop_watch
+#
+#
+# @stop_watch
+def solve(N, M, ab):
+    uf = UnionFind(N)
+    for a, b in ab:
+        uf.union(a - 1, b - 1)
+    print(uf.group_count() - 1)
+
+
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    ab = [[int(i) for i in input().split()] for _ in range(M)]
+    solve(N, M, ab)
+
+    # # test
+    # from random import randint
+    # import string
+    # import tool.testcase as tt
+    # from tool.testcase import random_str, random_ints
+    # solve()
