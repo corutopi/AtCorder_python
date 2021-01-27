fb5890c3443d7d21d48b8914061889186e27358e
corutopi <39874652+corutopi@users.noreply.github.com>
2021-01-28 01:31:48 +0900

    solve ABC 029 D.

--- AtCoderBeginnerContest/029/D.py
+++ AtCoderBeginnerContest/029/D.py
@@ -8,23 +8,29 @@ inf = float('inf')
 mod = 10 ** 9 + 7
 mod2 = 998244353
 
-from decorator import stop_watch
+# from decorator import stop_watch
+#
+#
+# @stop_watch
+def solve(N):
+    dp = [0, 1]
+    for i in range(13):
+        dp.append(dp[-1] * 10 + 10 ** (i + 1))
 
-
-@stop_watch
-def solve():
-    print('ans')
+    ans = 0
+    n = N
+    j = 0
+    while n > 0:
+        n, k = divmod(n, 10)
+        if k != 0:
+            ans += dp[j] * k + (N % (10 ** j) + 1 if k == 1 else 10 ** j)
+        j += 1
+    print(ans)
 
 
 if __name__ == '__main__':
-    # S = input()
-    # N = int(input())
-    # N, M = map(int, input().split())
-    # A = [int(i) for i in input().split()]
-    # B = [int(i) for i in input().split()]
-    # AB = [[int(i) for i in input().split()] for _ in range(N)]
-    # P = [int(input()) for _ in range(N)]
-    solve()
+    N = int(input())
+    solve(N)
 
     # # test
     # from random import randint
