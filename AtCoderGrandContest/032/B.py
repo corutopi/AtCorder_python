56d31242e9a26982e15cc7d15fcd996c80a98232
corutopi <39874652+corutopi@users.noreply.github.com>
2021-02-16 01:32:52 +0900

    solve AGC 032 B.

--- AtCoderGrandContest/032/B.py
+++ AtCoderGrandContest/032/B.py
@@ -1,18 +1,34 @@
-"""
-B - Balanced Neighbors
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# import bisect
+# from collections import deque
+# import string
+from math import ceil, floor
+inf = float('inf')
+mod = 10 ** 9 + 7
+mod2 = 998244353
 
-"""
-
-
-def solve():
-    N = int(input())
-    ans = [[] for n in range(N)]
-    while True:
-        max_list = [max(i) for i in ans]
-        max_list.index(max_list(max))
-        pass
-    pass
+# from decorator import stop_watch
+#
+#
+# @stop_watch
+def solve(N):
+    key = N + 1 if N % 2 == 0 else N
+    ans = []
+    for i, j in ((i, j) for i in range(1, N) for j in range(i + 1, N + 1)):
+        if i + j == key: continue
+        ans.append((str(i), str(j)))
+    print(len(ans))
+    [print(' '.join(a)) for a in ans]
 
 
 if __name__ == '__main__':
-    solve()
+    N = int(input())
+    solve(N)
+
+    # # test
+    # from random import randint
+    # import string
+    # import tool.testcase as tt
+    # from tool.testcase import random_str, random_ints
+    # solve()
