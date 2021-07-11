81da0aa3cbfa87444dbabad6d8ba57c9dcbc0d1a
corutopi <39874652+corutopi@users.noreply.github.com>
2021-07-11 23:30:46 +0900

    solve abc200 C(2 pattern).

---
+++ AtCoderBeginnerContest/200/C.py
@@ -0,0 +1,38 @@
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# import bisect
+# from collections import deque
+# import string
+from math import ceil, floor
+
+inf = float('inf')
+mod = 10 ** 9 + 7
+mod2 = 998244353
+
+# from decorator import stop_watch
+#
+#
+# @stop_watch
+def solve(N, A):
+    div = [0] * (200 + 1)
+    for a in A:
+        div[a % 200] += 1
+
+    ans = 0
+    for a in A:
+        div[a % 200] -= 1
+        ans += div[a % 200]
+    print(ans)
+
+
+if __name__ == '__main__':
+    N = int(input())
+    A = [int(i) for i in input().split()]
+    solve(N, A)
+
+    # # test
+    # from random import randint
+    # import string
+    # import tool.testcase as tt
+    # from tool.testcase import random_str, random_ints
+    # solve()
