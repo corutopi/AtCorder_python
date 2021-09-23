16f13da54d6ef87efb3fe58b1b4dd3ef109acebb
corutopi <39874652+corutopi@users.noreply.github.com>
2021-09-24 00:29:06 +0900

    solve abc218 D.

--- AtCoderBeginnerContest/218/D.py
+++ AtCoderBeginnerContest/218/D.py
@@ -1,4 +1,3 @@
-# 解説を参考に作成
 # import sys
 # sys.setrecursionlimit(10 ** 6)
 # import bisect
@@ -10,57 +9,34 @@ inf = float('inf')
 mod = 10 ** 9 + 7
 mod2 = 998244353
 
-
 # from decorator import stop_watch
 #
 #
 # @stop_watch
-def solve(N, S, T):
-    if sum(1 for i in range(N) for j in range(N) if S[i][j] == '#') != \
-            sum(1 for i in range(N) for j in range(N) if T[i][j] == '#'):
-        print('No')
-        return
-
-    for _ in range(4):
-        for i, j in ((i, j) for i in range(N) for j in range(N)):
-            if S[i][j] == '#':
-                Si, Sj = i, j
-                break
-
-        for i, j in ((i, j) for i in range(N) for j in range(N)):
-            if T[i][j] == '#':
-                Ti, Tj = i, j
-                break
-
-        offset_i = Ti - Si
-        offset_j = Tj - Sj
-
-        for i, j in ((i, j) for i in range(N) for j in range(N)):
-            if 0 <= i + offset_i < N and 0 <= j + offset_j < N:
-                if S[i][j] != T[i + offset_i][j + offset_j]:
-                    break
-            else:
-                if S[i][j] == '#':
-                    break
-        else:
-            print('Yes')
-            return
-
-        S = list(zip(*S[::-1]))
-
-    print('No')
+def solve(N, xy):
+    xy.sort(key=lambda x: x[0])
+    dict_xy = {str(xyi[0]) + '_' + str(xyi[1]): 1 for xyi in xy}
+    ans = 0
+    for i, j in ((i, j) for i in range(N) for j in range(i + 1, N)):
+        if xy[i][0] == xy[j][0] or xy[i][1] == xy[j][1]:
+            continue
+        a = str(xy[i][0]) + '_' + str(xy[j][1])
+        b = str(xy[j][0]) + '_' + str(xy[i][1])
+        if dict_xy.get(a, 0) + dict_xy.get(b, 0) == 2:
+            ans += 1
+    print(ans // 2)
 
 
 if __name__ == '__main__':
-    # S = input()
     N = int(input())
-    S = [input() for _ in range(N)]
-    T = [input() for _ in range(N)]
-    solve(N, S, T)
+    xy = [[int(i) for i in input().split()] for _ in range(N)]
+    solve(N, xy)
 
     # # test
     # from random import randint
     # import string
     # import tool.testcase as tt
     # from tool.testcase import random_str, random_ints
-    # solve()
+    # N = 2000
+    # xy = [[randint(0, 10), randint(0, 10)] for _ in range(N)]
+    # solve(N, xy)
