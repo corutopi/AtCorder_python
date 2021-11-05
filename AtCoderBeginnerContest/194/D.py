4d351d451e7a334907ca4976ea3af7b62d2afcb9
corutopi <39874652+corutopi@users.noreply.github.com>
2021-11-06 01:44:15 +0900

    solve abc194 D.

---
+++ AtCoderBeginnerContest/194/D.py
@@ -0,0 +1,36 @@
+# 以下を参考に作成
+#   https://manabitimes.jp/math/1053
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# import bisect
+# from collections import deque
+# import string
+from math import ceil, floor
+inf = float('inf')
+mod = 10 ** 9 + 7
+mod2 = 998244353
+
+# from decorator import stop_watch
+#
+#
+# @stop_watch
+def solve(N):
+    print(sum((1 / i) * N for i in range(1, N)))
+
+
+if __name__ == '__main__':
+    # S = input()
+    N = int(input())
+    # N, M = map(int, input().split())
+    # A = [int(i) for i in input().split()]
+    # B = [int(i) for i in input().split()]
+    # AB = [[int(i) for i in input().split()] for _ in range(N)]
+    # P = [int(input()) for _ in range(N)]
+    solve(N)
+
+    # # test
+    # from random import randint
+    # import string
+    # import tool.testcase as tt
+    # from tool.testcase import random_str, random_ints
+    # solve()
