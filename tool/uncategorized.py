b0e9820e1e5b4f40acf17f2734bb9d315621d613
corutopi <39874652+corutopi@users.noreply.github.com>
2021-11-25 00:36:34 +0900

    make slide_maximum_2d.

--- tool/uncategorized.py
+++ tool/uncategorized.py
@@ -176,7 +176,7 @@ def slide_minimum(l, k):
 
 
 def slide_maximum(l, k):
-    """スライド最大値のlistを返す1.
+    """スライド最大値のlistを返す.
     O(len(l))
 
     :param l: list
@@ -201,8 +201,63 @@ def slide_maximum(l, k):
     return re
 
 
+def slide_maximum_2d(l, h, w):
+    """2次元配列のスライド最大値のlistを返す.
+    各 l[i][j] 毎に, l[i][j], l[i + h][j + w]を頂点とする長方形内部での最大値を持つ2次元配列を作る.
+    計算量: O(len(l)*len(l[i])
+
+    :param l:
+    :param h:
+    :param w:
+    :return:
+    """
+    from collections import deque
+    H, W = len(l), len(l[0])
+    re = []
+
+    # 横方向の計算(slide_maximum 使ってもいいかもしれない)
+    for li in l:
+        x = []
+        dq = deque([])
+        for j in range(w):
+            while dq and li[dq[-1]] < li[j]:
+                dq.pop()
+            dq.append(j)
+        for j in range(W):
+            if j + w < W:
+                while dq and li[dq[-1]] < li[j + w]:
+                    dq.pop()
+                dq.append(j + w)
+            if dq[0] == j - 1:
+                dq.popleft()
+            x.append(li[dq[0]])
+        re.append(x)
+
+    # 縦方向の計算
+    for j in range(len(l[0])):
+        dq = deque([])
+        for i in range(h):
+            while dq and re[dq[-1]][j] < re[i][j]:
+                dq.pop()
+            dq.append(i)
+        for i in range(H):
+            if i + h < H:
+                while dq and re[dq[-1]][j] < re[i + h][j]:
+                    dq.pop()
+                dq.append(i + h)
+            if dq[0] == i - 1:
+                dq.popleft()
+            re[i][j] = re[dq[0]][j]
+
+    return re
+
+
 if __name__ == '__main__':
-    print(slide_minimum([9, 8, 7, 6, 5, 4, 3, 2, 1], 1))
-    print(slide_minimum([1, 2, 3, 4, 5, 6, 7, 8, 9], 2))
-    print(slide_maximum([9, 8, 7, 6, 5, 4, 3, 2, 1], 1))
-    print(slide_maximum([1, 2, 3, 4, 5, 6, 7, 8, 9], 2))
+    # print(slide_minimum([9, 8, 7, 6, 5, 4, 3, 2, 1], 1))
+    # print(slide_minimum([1, 2, 3, 4, 5, 6, 7, 8, 9], 2))
+    # print(slide_maximum([9, 8, 7, 6, 5, 4, 3, 2, 1], 1))
+    # print(slide_maximum([1, 2, 3, 4, 5, 6, 7, 8, 9], 2))
+    l = [[i * j for j in range(10)] for i in range(10)]
+    [print(l2) for l2 in l]
+    print('--------')
+    [print(sm) for sm in slide_maximum_2d(l, 2,3)]
