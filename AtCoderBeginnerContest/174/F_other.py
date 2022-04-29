0c1fff311b47e9b20b81d5ab90497358dfaee451
corutopi <39874652+corutopi@users.noreply.github.com>
2022-04-30 02:33:28 +0900

    solve abc174 F.

---
+++ AtCoderBeginnerContest/174/F_other.py
@@ -0,0 +1,122 @@
+"""
+解説AC
+"""
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# # for pypy
+# import pypyjit
+# pypyjit.set_param('max_unroll_recursion=-1')
+
+# import bisect
+# from collections import deque
+# import string
+from math import ceil, floor
+
+inf = float('inf')
+mod = 10 ** 9 + 7
+mod2 = 998244353
+
+
+class BinaryIndexedTree:
+    """
+    l = [1, 2, 3, 4, 5, 6, 7, 8] のlistを例とした場合、
+    以下のような範囲での演算結果(sum)を配列に持つ。
+        1: [1, 2, 3, 4, 5, 6, 7, 8]
+        2: [1, 2, 3, 4]
+        3: [1, 2]      [5, 6]
+        4: [1]   [3]   [5]   [7]
+    1 ～ r までの結果S(r)を、各層で必要な演算済みのデータを使うことで log(N) で計算できる.
+    l ～ r までの結果は S(r) - S(l - 1) で同じくlog(N)計算できる.
+    データ構造の作成は N*log(N).
+    配列データは1始まりとして計算.
+    長さ n + 1 (0 ~ n) の配列にデータを持ち, データ内の対象要素を l ~ r とすると, 配列の r 番目が格納先となる.
+    また対象要素の数は r の LSB(Least Significant Bit) に一致する.
+
+    転倒数の計算にも使える.
+    """
+
+    def __init__(self, n):
+        """
+        :param n: num of date.
+        """
+        self.num = n
+        self.tree = [0] * (n + 1)
+
+    def add(self, k, x):
+        """
+        :param k: [1, self.num]
+        :param x: add num.
+        :return: None
+        """
+        while k <= self.num:
+            self.tree[k] += x
+            k += k & -k
+
+    def sum(self, k):
+        """
+        1 ～ k までの合計
+        :param k:
+        :return:
+        """
+        re = 0
+        while k > 0:
+            re += self.tree[k]
+            k -= k & -k
+        return re
+
+    def sum_lr(self, l, r):
+        """
+        sum of form l to r
+        :param l: 1 <= l <= r
+        :param r: l <= r <= self.num
+        :return:
+        """
+        return self.sum(r) - self.sum(l - 1)
+
+
+class Query:
+    def __init__(self, i, l, r):
+        self.number = i
+        self.left = l
+        self.right = r
+
+
+# from decorator import stop_watch
+#
+#
+# @stop_watch
+def solve(N, Q, C, ILR):
+    bit = BinaryIndexedTree(N)
+    good_ball = [-1] * (N + 1)
+    ans = [0] * Q
+    j = 0
+    for q in ILR:
+        while j < q.right:
+            j += 1
+            c = C[j - 1]
+            if good_ball[c] != -1:
+                bit.add(good_ball[c], -1)
+            good_ball[c] = j
+            bit.add(good_ball[c], 1)
+        ans[q.number] = bit.sum_lr(q.left, q.right)
+    [print(a) for a in ans]
+
+
+if __name__ == '__main__':
+    N, Q = map(int, input().split())
+    C = [int(i) for i in input().split()]
+    tmp = [[] for _ in range(N + 1)]
+    for q in range(Q):
+        l, r = [int(i) for i in input().split()]
+        tmp[r].append(Query(q, l, r))
+    LR = []
+    for t in tmp:
+        LR += t
+    solve(N, Q, C, LR)
+
+    # # test
+    # from random import randint
+    # import string
+    # import tool.testcase as tt
+    # from tool.testcase import random_str, random_ints
+    # solve()
