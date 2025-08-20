cb8a6d6be0368b07404dcb4466ca5806639bd73a
corutopi <39874652+corutopi@users.noreply.github.com>
2025-08-21 01:32:41 +0900

    solve

---
+++ AtCoderBeginnerContest/2XX/297/B.py
@@ -0,0 +1,47 @@
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# import bisect
+# from collections import deque
+# import string
+from math import ceil, floor
+inf = float('inf')
+mod = 10 ** 9 + 7
+mod2 = 998244353
+
+# from decorator import stop_watch
+#
+#
+# @stop_watch
+def solve(S: str):
+    b1 = S.find('B')
+    b2 = S.find('B', b1 + 1)
+    if (b1 + b2) % 2 == 0:
+        print('No')
+        return
+
+    r1 = S.find('R')
+    r2 = S.find('R', r1 + 1)
+    k = S.find('K')
+    if not (r1 < k < r2):
+        print('No')
+        return
+
+    print('Yes')
+
+
+if __name__ == '__main__':
+    S = input()
+    # N = int(input())
+    # N, M = map(int, input().split())
+    # A = [int(i) for i in input().split()]
+    # B = [int(i) for i in input().split()]
+    # AB = [[int(i) for i in input().split()] for _ in range(N)]
+    # P = [int(input()) for _ in range(N)]
+    solve(S)
+
+    # # test
+    # from random import randint
+    # import string
+    # import tool.testcase as tt
+    # from tool.testcase import random_str, random_ints
+    # solve()
